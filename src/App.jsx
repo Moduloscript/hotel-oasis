6d92d24d8d51a5aef78fa7c6ddd76f52e7e7c525
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-02-06 22:00:53 +0100

    Develop the AppLayout Ui component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles"
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
@@ -8,25 +8,28 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import Bookings from "./pages/Bookings";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 const App = () => {
   return (
     <>
-    <GlobalStyles/>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to="dashboard"/>} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="cabins" element={<Cabins />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="users" element={<Users />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="account" element={<Account />} />
-        <Route path="login" element={<Login />} />
-        <Route path="" element={<PageNotFound />} />
-      </Routes>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="" element={<PageNotFound />} />
+        </Routes>
       </BrowserRouter>
-      </>
+    </>
   );
 };
 
