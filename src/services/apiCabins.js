d7eacd807c945635c161ddfd3a2b75ab1679d19b
Moduloscript <37522337+Moduloscript@users.noreply.github.com>
2024-02-08 06:35:13 +0100

    Setup and configured the supabase backend service 💻🛰📡

---
+++ src/services/apiCabins.js
@@ -0,0 +1,14 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data: cabins, error } = await supabase.from("cabins")
+    .select("*")
+    .range(0, 9);
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be found");
+  }
+
+  return cabins;
+}
